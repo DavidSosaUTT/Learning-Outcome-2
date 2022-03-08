c96d7a8d3e94dc0207d6d020da9527bb584ab748
DavidSosaUTT <david.moraless@uttn.mx>
2022-03-07 20:11:56 -0600

    first commit

---
+++ sw.js
@@ -0,0 +1,63 @@
+var cacheName = 'weatherPWA-v1';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/scripts/app.js',
+    '/scripts/localforage-1.4.0.js',
+    '/styles/ud811.css',
+    '/images/clear.png',
+    '/images/cloudy-scattered-showers.png',
+    '/images/cloudy.png',
+    '/images/fog.png',
+    '/images/ic_add_white_24px.svg',
+    '/images/ic_refresh_white_24px.svg',
+    '/images/partly-cloudy.png',
+    '/images/rain.png',
+    '/images/scattered-showers.png',
+    '/images/sleet.png',
+    '/images/snow.png',
+    '/images/thunderstorm.png',
+    '/images/wind.png'
+];
+
+//Esta funcion esta a la espera de que se instale el Service Worker. Cuando este sea instalado, este codigo va 
+//a comenzar a guardar el cache de nuestra aplicacion en un archivo .log
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+//Esta funcion espera a que se active el Service Worker. Cuando esto suceda borrara el 
+//cache almacenado de pasadas versiones si es que hay alguno.
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList){
+            return Promise.all(keyList.map(function(key){
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+//Esta funcion trata de recuperar el cache y revisar si hay cambios que actualizar, si el cache actual 
+//es el mismo que el recuperado con esta funcion, entonces todo correcto (algo asi? u.u)
+
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
